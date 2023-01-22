8325c60f3b31162ee641478f0460ed732ec2f7be
91840 <91840@LAPTOP-M3HSMS1J>
2023-01-22 12:18:48 +0530

    //Tescase 4 added

---
+++ src/test/java/Practice/TestCase4.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase4 {
+@Test
+
+public void TestCaseFour()
+{
+	Reporter.log("TestCase 4 called",true);
+	}
+}
